b6f31658b9d4f9efd18ea7c6cd76b4f36dc584b7
David Lacarta <davidlacarta@gmail.com>
2021-10-25 19:16:53 +0200

    feat: Add gross to net salary simplified

--- src/grossToNetSalary.test.ts
+++ src/grossToNetSalary.test.ts
@@ -1,13 +1,33 @@
 import grossToNetSalary from "./grossToNetSalary";
 
-test("With annual gross salary 30.000 should calculate the net salary", () => {
-  expect(grossToNetSalary({ annualGrossSalary: 30000 })).toStrictEqual({
-    monthlyNetSalary: 1929.8,
-  });
-});
+const cases = [
+  {
+    input: { annualGrossSalary: 30000 },
+    expected: { monthlyNetSalary: 1929.8 },
+  },
+  {
+    input: { annualGrossSalary: 45000 },
+    expected: { monthlyNetSalary: 2720.3 },
+  },
+  {
+    input: { annualGrossSalary: 10000 },
+    expected: { monthlyNetSalary: 766.5 },
+  },
+  {
+    input: { annualGrossSalary: 20000 },
+    expected: { monthlyNetSalary: 1366 },
+  },
+  {
+    input: { annualGrossSalary: 60000 },
+    expected: { monthlyNetSalary: 3507.8 },
+  },
+];
 
-test("With annual gross salary 45.000 should calculate the net salary", () => {
-  expect(grossToNetSalary({ annualGrossSalary: 45000 })).toStrictEqual({
-    monthlyNetSalary: 2720.3,
-  });
-});
+cases.forEach(({ input, expected }) =>
+  test(
+    "With annual gross salary " +
+      input.annualGrossSalary +
+      " should calculate the net salary",
+    () => expect(grossToNetSalary(input)).toStrictEqual(expected)
+  )
+);
