5fd0c9f714ef8da511443b29be9fad84e7c2e769
David Lacarta <davidlacarta@gmail.com>
2021-10-26 21:17:52 +0200

    style: Add tailwindcss styles

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./*.html", "./src/**/*.ts"],
+  darkMode: "media",
+  theme: {
+    colors: {
+      white: colors.white,
+      gray: colors.gray,
+      teal: colors.teal,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
