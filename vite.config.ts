f3e0a6a73156255e3c9e2d9634796ef49cfe24c9
David Lacarta <davidlacarta@gmail.com>
2021-11-12 16:42:51 +0100

    Add meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 
+const sourceDirectory = resolve(__dirname, "src");
+
 export default defineConfig({
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "src"),
+      partialDirectory: sourceDirectory,
       reloadOnPartialChange: false,
     }),
   ],
