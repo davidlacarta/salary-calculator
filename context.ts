68c04513981a520fb3bbfcbce48e13535f25225f
David Lacarta <davidlacarta@gmail.com>
2021-11-12 20:53:52 +0100

    Add environment vars and context

---
+++ context.ts
@@ -0,0 +1,5 @@
+export default {
+  title: "Calculadora de sueldo",
+  description:
+    "Calcula cuánto cobrarás como sueldo neto anual o mensual según la cantidad que recibas en bruto.",
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import handlebars from "vite-plugin-handlebars";
+import context from "./context";
 
 const sourceDirectory = resolve(__dirname, "src");
 
-export default defineConfig({
-  plugins: [
-    handlebars({
-      partialDirectory: sourceDirectory,
-      reloadOnPartialChange: false,
-    }),
-  ],
-});
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+
+  return defineConfig({
+    plugins: [
+      handlebars({
+        partialDirectory: sourceDirectory,
+        reloadOnPartialChange: false,
+        context: {
+          VITE_BASE_URL: process.env.VITE_BASE_URL || "/",
+          VITE_UMAMI_SRC: process.env.VITE_UMAMI_SRC,
+          VITE_UMAMI_DOMAINS: process.env.VITE_UMAMI_DOMAINS,
+          VITE_UMAMI_WEBSITE_ID: process.env.VITE_UMAMI_WEBSITE_ID,
+          ...context,
+        },
+      }),
+    ],
+  });
+};
