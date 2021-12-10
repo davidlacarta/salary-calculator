a4861da1eb5941e4ce5ae5adec3f4f97c5857089
David Lacarta <davidlacarta@gmail.com>
2021-12-10 18:08:03 +0100

    feat: :sparkles: Add github repository link

--- context.ts
+++ context.ts
@@ -1,4 +1,5 @@
 export default {
+  github: "https://github.com/davidlacarta/salary-calculator",
   title: "Calculadora de sueldo",
   description:
     "Calcula cuánto cobrarás como sueldo neto anual o mensual según la cantidad que recibas en bruto.",
